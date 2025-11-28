41e2e0a529ebdb83eab079240c99146037847944
xandfcosta <fcosta.xand@gmail.com>
2025-11-28 11:58:59 -0300

    fix: webphone was opening with a ghost notification

--- src/components/layout/status-bar/Notifications.tsx
+++ src/components/layout/status-bar/Notifications.tsx
@@ -54,60 +54,61 @@ export function Notifications() {
       </PopoverTrigger>
       <PopoverContent className="wv:flex wv:flex-col wv:max-h-[200px] wv:w-[400px] wv:overflow-y-scroll wv:p-2">
         <div className="wv:flex wv:flex-col">
-          {notifications.slice(0, items).map((notification) => (
-            <div
-              className={`wv:bg-background  ${notification.isHidden ? " notification exit" : "wv:flex notification"}  wv:flex-col wv:p-1 wv:gap-2`}
-              key={`notification_${notification.id}`}
-            >
-              <div className="wv:flex wv:flex-row wv:gap-2 wv:full">
-                <div className="wv:bg-background/50 wv:relative wv:flex wv:flex-row wv:w-8 wv:h-8 wv:justify-center wv:items-center wv:rounded-full ">
-                  <PhoneOutgoing size={15} />
-                  <Badge
-                    className="wv:absolute wv:bottom-0 wv:right-0 wv:h-3 wv:w-3 wv:rounded-full wv:px-[1px] wv:bg-red-400"
-                    variant="destructive"
-                  >
-                    <X size={20} />
-                  </Badge>
-                </div>
+          {hasNotification &&
+            notifications.slice(0, items).map((notification) => (
+              <div
+                className={`wv:bg-background  ${notification.isHidden ? " notification exit" : "wv:flex notification"}  wv:flex-col wv:p-1 wv:gap-2`}
+                key={`notification_${notification.id}`}
+              >
+                <div className="wv:flex wv:flex-row wv:gap-2 wv:full">
+                  <div className="wv:bg-background/50 wv:relative wv:flex wv:flex-row wv:w-8 wv:h-8 wv:justify-center wv:items-center wv:rounded-full ">
+                    <PhoneOutgoing size={15} />
+                    <Badge
+                      className="wv:absolute wv:bottom-0 wv:right-0 wv:h-3 wv:w-3 wv:rounded-full wv:px-[1px] wv:bg-red-400"
+                      variant="destructive"
+                    >
+                      <X size={20} />
+                    </Badge>
+                  </div>
 
-                <div className="wv:flex wv:flex-col wv:flex-grow wv:gap-1">
-                  {notification.type === "CALL_FAILED" && (
-                    <p className="wv:text-[14px] wv:leading-none">Ligação falhou</p>
-                  )}
-
-                  <p className="wv:text-[10px] wv:truncate wv:max-w-[100px]">{notification.token}</p>
-                </div>
-
-                <div className="wv:flex wv:flex-row wv:justify-start wv:items-start">
-                  <div className="wv:flex wv:flex-row wv:justify-center wv:items-center wv:gap-1">
-                    {!notification.isRead && (
-                      <Badge
-                        className={`${notification.isRead ? " notificationView exit" : "wv:flex notificationView "}  wv:h-2 wv:w-2 wv:rounded-full wv:px-[1px] wv:bg-blue-500`}
-                        variant="destructive"
-                      />
+                  <div className="wv:flex wv:flex-col wv:flex-grow wv:gap-1">
+                    {notification.type === "CALL_FAILED" && (
+                      <p className="wv:text-[14px] wv:leading-none">Ligação falhou</p>
                     )}
 
-                    <p className="wv:text-[12px] wv:opacity-90 wv:truncate wv:max-w-[100px]">
-                      {moment(notification.created_at).locale("pt-br").fromNow()}
-                    </p>
-                    <Button
-                      type="button"
-                      variant={"ghost"}
-                      className=" wv:[&_svg:not([class*=size-])]:size-2  wv:!p-0.5 wv:aspect-square wv:w-4 wv:h-4"
-                      onClick={() => removeNotification(notification.id)}
-                    >
-                      <XIcon size={2} />
-                    </Button>
+                    <p className="wv:text-[10px] wv:truncate wv:max-w-[100px]">{notification.token}</p>
+                  </div>
+
+                  <div className="wv:flex wv:flex-row wv:justify-start wv:items-start">
+                    <div className="wv:flex wv:flex-row wv:justify-center wv:items-center wv:gap-1">
+                      {!notification.isRead && (
+                        <Badge
+                          className={`${notification.isRead ? " notificationView exit" : "wv:flex notificationView "}  wv:h-2 wv:w-2 wv:rounded-full wv:px-[1px] wv:bg-blue-500`}
+                          variant="destructive"
+                        />
+                      )}
+
+                      <p className="wv:text-[12px] wv:opacity-90 wv:truncate wv:max-w-[100px]">
+                        {moment(notification.created_at).locale("pt-br").fromNow()}
+                      </p>
+                      <Button
+                        type="button"
+                        variant={"ghost"}
+                        className=" wv:[&_svg:not([class*=size-])]:size-2  wv:!p-0.5 wv:aspect-square wv:w-4 wv:h-4"
+                        onClick={() => removeNotification(notification.id)}
+                      >
+                        <XIcon size={2} />
+                      </Button>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="wv:flex wv:flex-col">
-                <p className="wv:text-[12px] wv:opacity-50 wv:break-all ">{notification.detail}</p>
-                <p className="wv:text-[12px] wv:opacity-50 wv:break-all ">{notification.message}</p>
+                <div className="wv:flex wv:flex-col">
+                  <p className="wv:text-[12px] wv:opacity-50 wv:break-all ">{notification.detail}</p>
+                  <p className="wv:text-[12px] wv:opacity-50 wv:break-all ">{notification.message}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
 
           {!hasNotification && <p className="wv:text-center wv:pt-2">Nenhuma notificação</p>}
 
