83c05aa851e3c7661f560d565d966c58fb7ca2e2
xandfcosta <fcosta.xand@gmail.com>
2026-05-28 14:39:58 -0300

    fix: use global variable __WEBPHONE_VERSION__ as label on webphone

--- src/components/WebPhone.tsx
+++ src/components/WebPhone.tsx
@@ -6,7 +6,6 @@ import { useWidget } from "@/providers/WidgetProvider";
 import CallScreen from "@/screens/CallScreen";
 import KeyboardScreen from "@/screens/KeyboardScreen";
 import OutgoingScreen from "@/screens/OutgoingScreen";
-import pkg from "../../package.json";
 
 export function WebPhone() {
   const middleware = useMiddleware();
@@ -44,7 +43,7 @@ export function WebPhone() {
           className="wv:text-neutral-500 pointer-events-none wv:absolute wv:bottom-1 wv:left-2 wv:select-none wv:z-50 wv:text-[12px]"
           aria-hidden="true"
         >
-          v {pkg.version}
+          v {__WEBPHONE_VERSION__}
         </p>
       </div>
     </>
